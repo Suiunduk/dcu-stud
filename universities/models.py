eb8272a1fbf0398967770c1d440af5cde7a84d68
Suiunduk <suyunduknktl@gmail.com>
2020-08-12 15:29:52 +0600

    Initial commit

---
+++ universities/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+from django.urls import reverse
+from django.utils import timezone
+
+
+class University(models.Model):
+    university_name = models.CharField('Название учебного заведения', max_length=200)
+    university_address = models.CharField('Фактический адрес учебного заведения', max_length=200)
+
+    def __str__(self):
+        return f'{self.university_name}'
+
+    #def get_absolute_url(self):
+    #    return reverse('university-detail', kwargs={'pk': self.pk})
